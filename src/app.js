00c91d94f6672b8d026bafa86eae65c9e5da373d
X-Anas-X <anas.zain1996@gmail.com>
2020-07-06 22:13:13 +0300

    done lab31

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import ToDo from './components/todo/todo.js';
+import ToDo from './todo/todo.js';
+import Header from './todo/header'
 
 function App () {
     return (
         <>
+        <Header />
         <ToDo />
         </>
     );
