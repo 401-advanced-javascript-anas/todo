5566b5474d10021999f44cdf047d9ef4d43098da
X-Anas-X <anas.zain1996@gmail.com>
2020-07-07 23:00:37 +0300

    Updates

---
+++ precache-manifest.042996f6b46d27fe0d3b190ef4cc66ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06955afe610f39ce001ec7f7b59e0c87",
+    "url": "/401-advanced-javascript-anas/todo/index.html"
+  },
+  {
+    "revision": "b133a94a0808803dbdc7",
+    "url": "/401-advanced-javascript-anas/todo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "81b92436235091ae1b5e",
+    "url": "/401-advanced-javascript-anas/todo/static/css/main.b06360a0.chunk.css"
+  },
+  {
+    "revision": "b133a94a0808803dbdc7",
+    "url": "/401-advanced-javascript-anas/todo/static/js/2.c0ff4630.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/401-advanced-javascript-anas/todo/static/js/2.c0ff4630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81b92436235091ae1b5e",
+    "url": "/401-advanced-javascript-anas/todo/static/js/main.64b43fab.chunk.js"
+  },
+  {
+    "revision": "421bc9b512a480ba32ad",
+    "url": "/401-advanced-javascript-anas/todo/static/js/runtime-main.fa55c750.js"
+  }
+]);
